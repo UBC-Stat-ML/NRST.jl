d1cbc35f730bf45bda0f2dd962ddac61846e7714
Miguel Biron <miguelbl9@gmail.com>
2022-04-14 15:57:11 -0700

    fixed betas tuning prototype

--- test/playground_Turing.jl
+++ test/playground_Turing.jl
@@ -3,6 +3,7 @@
 #########################################################################################
 
 using Random, Distributions, DynamicPPL, NRST, Plots, StatsBase
+using Interpolations, Roots
 
 # lognormal prior for variance, normal likelihood
 @model function Lnmodel(x)
@@ -10,7 +11,7 @@ using Random, Distributions, DynamicPPL, NRST, Plots, StatsBase
     x .~ Normal(0.,s)
 end
 lnmodel = Lnmodel(randn(30)) # -.1234
-betas = range(0,1,15) .^(2)
+betas = pushfirst!(2 .^(range(-8,0,19)), 0.)
 ns=NRSTSampler(
     lnmodel, # use the Turing model to build potentials and prior sampling
     betas,
@@ -25,17 +26,15 @@ par_res = run!(samplers, ntours = 5000);
 # [get_num_produce(s.np.fns.viout) for s in samplers] # they should be different
 oldTE = par_res.toureff
 
-# estimate
-at = 0:ns.np.N
-h=(x->x[1])
-infdf = inference(par_res, h=h, at=at)
-show(infdf, allrows=true)
+# # estimate
+# at = 0:ns.np.N
+# h=(x->x[1])
+# infdf = inference(par_res, h=h, at=at)
+# show(infdf, allrows=true)
 
 # estimate Λ
-using Interpolations, Roots
-
 rejrat = par_res.rejecs ./ par_res.visits 
-averej = 0.5(rejrat[1:(end-1),1] + rejrat[2:end,1])
+averej = 0.5(rejrat[1:(end-1),1] + rejrat[2:end,2]) # average outgoing and incoming rejections
 Λvals = pushfirst!(cumsum(averej),0.)
 Λvalsnorm = Λvals/Λvals[end]
 # plot(betas,Λvalsnorm)
@@ -45,22 +44,39 @@ sum(abs, Λnorm.(betas) - Λvalsnorm) < 10eps()
 
 # find betas by inverting Λnorm and u
 # initialize
-b1 = 0.
 Δ  = 1/ns.np.N # equal steps
 targetΛ = 0.
 newbetas = similar(betas)
-newbetas[1] = 0.
+newbetas[1] = minimum(betas)
 for i in 2:ns.np.N
     targetΛ += Δ
     b1 = newbetas[i-1]
-    b2 = betas[findfirst(u -> (u>targetΛ), Λvalsnorm)] # Λnorm^{-1}(targetΛ) cannot exceed this
+    b2 = min(1.,betas[findfirst(u -> (u>targetΛ), Λvalsnorm)]) # Λnorm^{-1}(targetΛ) cannot exceed this
     newbetas[i] = find_zero(β -> Λnorm(β)-targetΛ, (b1,b2), atol = 0.01*Δ) # set tolerance |Λnorm(β)-target|< 5%Δ 
 end
 newbetas[ns.np.N+1] = 1.
-copyto!(ns.np.betas, newbetas)
-samplers[2].explorers[1].U.betas # change propagates, cause its just pointers!
-NRST.initialize_c!(ns,nsteps=1000)
-res = post_process(run!(ns,nsteps=50000));
-plot(0:ns.np.N, vec(sum(res.visits,dims=2)))
-par_res = run!(samplers, ntours = 5000);
-par_res.toureff
+
+# visualize the change
+p1 = plot(x->Λnorm(x), 0., 1., label = "Λnorm", legend = :topleft, xlim=(0.,1.), ylim=(0.,1.))
+plot!(p1, [0.,0.], [0.,0.], label="βold", color = :green)
+for (i,b) in enumerate(betas[2:end])
+    y=Λvalsnorm[i+1]
+    plot!(p1, [b,b], [0.,y], label="", color = :green) # vertical
+    plot!(p1, [0,b], [y,y], label="", color = :green, linestyle = :dot)  # horizontal
+end
+p2 = plot(x->Λnorm(x), 0., 1., label = "Λnorm", legend = :topleft, xlim=(0.,1.), ylim=(0.,1.))
+plot!(p2, [0.,0.], [0.,0.], label="βnew", color = :green)
+for (i,b) in enumerate(newbetas[2:end])
+    y=(0:Δ:1)[i+1]
+    plot!(p2, [b,b], [0.,y], label="", color = :green) # vertical
+    plot!(p2, [0,b], [y,y], label="", color = :green, linestyle = :dot)  # horizontal
+end
+plot(p1,p2,layout=(2,1))
+maximum(abs,betas-newbetas)
+copyto!(ns.np.betas, newbetas)   # this propagates even to "betas" in main module
+NRST.tune_explorers!(ns,nsteps=500)
+NRST.initialize_c!(ns,nsteps=8*10*ns.nexpl) # should double in every iteration
+par_res = run!(samplers, ntours = 5000);    # this too
+plot(0:ns.np.N, vec(sum(par_res.visits,dims=2)))
+par_res.toureff .> oldTE
+
