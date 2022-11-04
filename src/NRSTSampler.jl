19745712e674e17cc52ba382c2b26538cb810691
Miguel Biron-Lattes <miguelbl9@gmail.com>
2022-11-04 09:35:58 -0700

    disable threaded tour runs

--- src/NRSTSampler.jl
+++ src/NRSTSampler.jl
@@ -299,7 +299,8 @@ function parallel_run(
     res  = [NRSTTrace(T,ns.np.N,TF) for _ in 1:ntours]                        # get one empty trace for each task
     rngs = [split(rng) for _ in 1:ntours]                                     # split rng into ntours copies. must be done outside of loop because split changes rng state.
     p    = ProgressMeter.Progress(ntours; desc="Sampling: ", enabled=verbose) # prints a progress bar
-    Threads.@threads for t in 1:ntours
+    for t in 1:ntours                                                         # debug
+    # Threads.@threads for t in 1:ntours
         tour!(copy(ns), rngs[t], res[t]; keep_xs=keep_xs, kwargs...)          # run a tour with tasks' own sampler, rng, and trace, avoiding race conditions. note: writing to separate locations in a common vector is fine. see: https://discourse.julialang.org/t/safe-loop-with-push-multi-threading/41892/6, and e.g. https://stackoverflow.com/a/8978397/5443023
         ProgressMeter.next!(p)
     end
