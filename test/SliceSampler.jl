b6163363e179f1708f45f3ae309ae65c90cbe508
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-02-17 11:21:56 -0800

    improve SS test

--- test/SliceSampler.jl
+++ test/SliceSampler.jl
@@ -1,19 +1,27 @@
 @testset "SliceSampler.jl" begin
     @model function ToyModel()
-        y ~ Normal()
+        y1 ~ Normal(7,0.1)
+        y2 ~ Laplace(-3,5)
+        y3 ~ Gumbel()
     end
     tm  = NRST.TuringTemperedModel(ToyModel())
-    rng = SplittableRandom()
-    x   = [randn(rng)]
+    rng = SplittableRandom(1)
+    x   = rand(tm,rng)
     ps  = NRST.potentials(tm,x)
     ss  = NRST.SliceSampler(
         tm, x, Ref(1.0), Ref(ps[1]), Ref(0.0), Ref(ps[1])
     );
-    nsteps=30000
-    xs = similar(x, nsteps)
-    for i in 1:nsteps
-        NRST.step!(ss,rng)
-        xs[i] = ss.x[1]
-    end
-    @test pvalue(OneSampleADTest(xs, Normal())) > 0.1
+    nxs = 10000
+    xs  = collect(hcat(map(_ -> (NRST.step!(ss,rng); copy(ss.x)),1:nxs)...)');
+    @test all(
+        [
+            pvalue(
+                ApproximateOneSampleKSTest(
+                    xs[:,i], 
+                    first(getproperty(tm.viout.metadata,sym).dists) 
+                )
+            ) 
+        for (i,sym) in enumerate(propertynames(tm.viout.metadata))
+        ] .> 0.01
+    )
 end
